e0fea28a52a126a809f30eb1aeb5d7e8ed646316
jawad Bin Azam <abidibnazam@gmail.com>
2023-06-12 19:55:03 +0600

    get request from users collection

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ async function run() {
     });
 
     // api for users
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };

07b691f4e4865f9f1c9cc601c631f39a8f53ef04
jawad Bin Azam <abidibnazam@gmail.com>
2023-06-12 19:52:17 +0600

    prevented google sign in/up data from inserting twice

--- index.js
+++ index.js
@@ -55,6 +55,11 @@ async function run() {
     // api for users
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
